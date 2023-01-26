7c768d5e0887ab4983799c2ce052ba9616a0cbb1
Nurzhan <kadyrbekova382@gmail.com>
2023-01-26 23:37:22 +0600

    SwiftPayment

--- src/main/java/swiftpayment/SwiftPayment.java
+++ src/main/java/swiftpayment/SwiftPayment.java
@@ -1,7 +1,8 @@
-package org.example;
+package swiftpayment;
 
 public class SwiftPayment {
     public static void main(String[] args) {
         System.out.println("This is SWIFT payment");
+        System.out.println("This is test");
     }
 }
